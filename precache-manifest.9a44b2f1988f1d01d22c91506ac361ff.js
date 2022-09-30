ba7c8f74bbd5395f12f9d1b8e5b76e23523443f8
swetank12 <swetank@settlemint.com>
2022-09-30 15:58:43 +0530

    Updates

---
+++ precache-manifest.9a44b2f1988f1d01d22c91506ac361ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2af73e201eec56f0bd85",
+    "url": "/React-Project/static/css/main.0abb8d32.chunk.css"
+  },
+  {
+    "revision": "2af73e201eec56f0bd85",
+    "url": "/React-Project/static/js/main.2af73e20.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/React-Project/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/React-Project/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "cec2a539231100ab5b04",
+    "url": "/React-Project/static/js/runtime~main.cec2a539.js"
+  },
+  {
+    "revision": "b3fcc46cee0a13cc0b64a5f797dcb18e",
+    "url": "/React-Project/index.html"
+  }
+];
